5a34985553bf927c17585fa8aa4ae0194a47df98
GitMatkta <zy15zz@student.aau.dk>
2026-02-05 14:50:55 +0100

    add mandelbrot skeleton

---
+++ mandelbrot.py
@@ -0,0 +1,22 @@
+from doctest import Example
+
+
+Mandelbrot Set Generator
+Author : [ Magnus ]
+Course : Numerical Scientific Computing 2026
+
+def f(x):
+    
+    Example function .
+    Parameters
+    
+    x = (-2,1)
+    y = (-1.5,1.5)
+
+    Returns
+    
+    float
+    Output value
+
+    # TODO : Implement the algorithm
+    pass
